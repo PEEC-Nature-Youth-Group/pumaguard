7a4c71158d486b24c0d866cb53d0c0b9ccf96628
Adis Bock <adisbock@gmail.com>
2025-04-03 09:55:16 -0600

    Add sounds and play sounds in server
    Co-authored-by: Adis Bock <adisbock@gmail.com>
    Co-authored-by: AdityaVis123 <hviwana@yahoo.com>
    Signed-off-by: Adis Bock <adisbock@gmail.com>

--- scripts/play_sound.py
+++ scripts/play_sound.py
@@ -1,12 +1,25 @@
-from playsound3 import playsound
+"""
+Test script to play a sound file using the USB speaker.
+"""
+
+import os
+
+from playsound3 import (
+    playsound,
+)
+
 
 def play_sound(file_path):
     """
     Play a sound file using the USB speaker.
-    
+
     Args:
         file_path (str): Path to the sound file.
     """
     playsound(file_path)
 
-play_sound('/home/peec/pumaguard/scripts/forest-ambience-296528.mp3')
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+sound_file_path = os.path.join(
+    script_dir, '../sounds/forest-ambience-296528.mp3')
+play_sound(sound_file_path)

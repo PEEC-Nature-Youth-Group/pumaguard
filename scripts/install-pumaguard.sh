1f65ab520d7492617c0f510b78896d1d8114e1fc
Nicolas Bock <nicolasbock@gmail.com>
2025-06-16 15:56:34 -0600

    Cleaning up
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- scripts/install-pumaguard.sh
+++ scripts/install-pumaguard.sh
@@ -6,8 +6,10 @@ sudo apt-get update
 sudo apt-get install --yes --no-install-recommends python3-pip python3-venv
 
 python3 -m venv venv
-./venv/bin/pip install --upgrade pip
-./venv/bin/pip install ansible passlib pumaguard
+source venv/bin/activate
+
+pip install --upgrade pip
+pip install ansible passlib pumaguard
 
 curl --output deploy-pumaguard.yaml https://raw.githubusercontent.com/PEEC-Nature-Youth-Group/pumaguard/refs/heads/main/scripts/deploy-pumaguard.yaml
 curl --output vsftpd.conf.j2 https://raw.githubusercontent.com/PEEC-Nature-Youth-Group/pumaguard/refs/heads/main/scripts/vsftpd.conf.j2
@@ -18,4 +20,4 @@ echo "#######################"
 echo
 
 echo "Please set a password for the ftpuser in laptop_config.yaml and run the following command to deploy Pumaguard:"
-echo "./venv/bin/ansible-playbook --connection local deploy-pumaguard.yaml"
+echo "${HOME}/venv/bin/ansible-playbook --connection local deploy-pumaguard.yaml"

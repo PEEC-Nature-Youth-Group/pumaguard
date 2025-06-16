186bdf4e11e65a9aaafac3ef52dff20ec46b57a6
Nicolas Bock <nicolasbock@gmail.com>
2025-06-16 15:41:16 -0600

    Make output a bit clearer
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- scripts/install-pumaguard.sh
+++ scripts/install-pumaguard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e -u -x
+set -e -u
 
 sudo apt-get update
 sudo apt-get install --yes --no-install-recommends python3-pip python3-venv
@@ -12,6 +12,10 @@ python3 -m venv venv
 curl --output deploy-pumaguard.yaml https://raw.githubusercontent.com/PEEC-Nature-Youth-Group/pumaguard/refs/heads/main/scripts/deploy-pumaguard.yaml
 curl --output vsftpd.conf.j2 https://raw.githubusercontent.com/PEEC-Nature-Youth-Group/pumaguard/refs/heads/main/scripts/vsftpd.conf.j2
 curl --output laptop_config.yaml https://raw.githubusercontent.com/PEEC-Nature-Youth-Group/pumaguard/refs/heads/main/scripts/laptop_config.yaml
+
+echo
+echo "#######################"
+echo
 
 echo "Please set a password for the ftpuser in laptop_config.yaml and run the following command to deploy Pumaguard:"
 echo "./venv/bin/ansible-playbook --connection local --ask-become-pass deploy-pumaguard.yaml"

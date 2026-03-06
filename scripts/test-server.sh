2057d84c0253b9190b795a0ef8885ee5c27fa5c9
Nicolas Bock <nicolasbock@gmail.com>
2026-03-06 15:23:58 -0700

    Remove tensorflow
    Since we switched to using ultralytics, tensorflow is not needed
    anymore.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- scripts/test-server.sh
+++ scripts/test-server.sh
@@ -28,23 +28,23 @@ SERVER_PID=$!
 
 api_available=0
 echo -n "Waiting for API to report status"
-while true; do
+for i in {1..20}; do
     if [[ $(curl --silent http://localhost:5000/api/status | jq --raw-output .status) == running ]]; then
         break
     fi
-    sleep 1
+    sleep 2
 done
 echo
 
 observer_started=0
 echo -n "Waiting for server to start observer"
-for i in {1..60}; do
+for i in {1..20}; do
     if grep "New observer started" ${LOGFILE}; then
         observer_started=1
         break
     fi
     echo -n .
-    sleep 1
+    sleep 2
 done
 echo
 if (( observer_started != 1 )); then
@@ -56,13 +56,13 @@ cp training-data/verification/lion/IMG_9177.JPG watchfolder/
 
 image_found=0
 echo -n "Check whether server identifies image"
-for i in {1..60}; do
+for i in {1..20}; do
     if grep "Chance of puma in watchfolder/IMG_9177.JPG" ${LOGFILE}; then
         image_found=1
         break
     fi
     echo -n .
-    sleep 1
+    sleep 2
 done
 echo
 

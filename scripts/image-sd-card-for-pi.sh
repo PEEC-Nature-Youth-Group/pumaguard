9c0999db33f54c2186d482fac5fc56ca062de527
Nicolas Bock <nicolasbock@gmail.com>
2026-09-24 10:04:35 -0600

    Add key directly to user-data
    If there is not network connection, we can't pull the key from
    launchpad. Add it as hardcoded string instead.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- scripts/image-sd-card-for-pi.sh
+++ scripts/image-sd-card-for-pi.sh
@@ -98,5 +98,6 @@ users:
       passwd: ${HASHED}
       lock_passwd: false
 runcmd:
-    - sudo -u pumaguard ssh-import-id lp:nicolasbock
+    - sudo -u pumaguard -i mkdir --parents .ssh
+    - echo "$(cat ~/.ssh/id_rsa.pub)" | sudo -u pumaguard -i tee --append .ssh/authorized_keys
 EOF
